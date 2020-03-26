3ff7229f181a1d646df9a661ff200f8382801f84
Stoian, Cosmin-Adrian <cstoian@deloittece.com>
2020-03-26 22:18:08 +0200

    Improvements, refactoring, cleaning up...

--- src/commands/metadatautil/upgradeapiversion.ts
+++ src/commands/metadatautil/upgradeapiversion.ts
@@ -1,66 +1,90 @@
-import { flags, SfdxCommand } from '@salesforce/command';
-import { Messages } from '@salesforce/core';
-import { AnyJson } from '@salesforce/ts-types';
-import {BaseRequest} from '../../services/interfaces'
-import {ModifyMetaXMLFile} from '../../services/modifymetaxmlfile'
+import { flags, SfdxCommand } from "@salesforce/command";
+import { Messages } from "@salesforce/core";
+import { BaseRequest } from "../../services/interfaces";
+import { ModifyMetaXMLFile } from "../../services/modifymetaxmlfile";
+import * as constants from "../../constants/constants";
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
-
-// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
-// or any library that is using the messages framework can also be loaded this way.
-const messages = Messages.loadMessages('salesforce-apiversion-upgrade', 'org');
+const messages = Messages.loadMessages(
+  "salesforce-apiversion-upgrade",
+  "messages"
+);
 
 export default class UpgradeAPIVersion extends SfdxCommand {
-
-  //public static description = messages.getMessage('commandDescription');
-
-  public static examples = [
-  `$ sfdx hello:org --targetusername myOrg@example.com --targetdevhubusername devhub@org.com
-  Hello world! This is org: MyOrg and I will be around until Tue Mar 20 2018!
-  My hub org id is: 00Dxx000000001234
-  `,
-  `$ sfdx hello:org --name myname --targetusername myOrg@example.com
-  Hello myname! This is org: MyOrg and I will be around until Tue Mar 20 2018!
-  `
-  ];
-
-  public static args = [{name: 'file'}];
-
-  protected static flagsConfig = {
-    metadata: flags.string({char: 'm', description: messages.getMessage('metadatatype')}),
-    sourceversion: flags.number({char: 's', description: messages.getMessage('sourceapi')}),
-    targetversion: flags.number({char: 't', description: messages.getMessage('targetapi')}),//behaviour changes
-    fileprefix: flags.string({char: 'x', description: messages.getMessage('prefix')}),//regex pattern
-    path: flags.string({char: 'p', description: messages.getMessage('path')})
-  };
-
-  // Comment this out if your command does not require an org username
+  // SFDX CLI specific configuration
   protected static requiresUsername = false;
-
-  // Comment this out if your command does not support a hub org username
-  protected static supportsDevhubUsername = true;
-
-  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static supportsDevhubUsername = false;
   protected static requiresProject = false;
 
-  public async run(): Promise<AnyJson> {
-    this.ux.styledHeader( messages.getMessage('pluginTitle') );
-		
-		// Run the requested action
-		let baseRequest : BaseRequest = {
+  // Define flags and options
+  protected static flagsConfig = {
+    metadata: flags.array({
+      char: "m",
+      description: messages.getMessage("metadatatype"),
+      required: true,
+      delimiter: "/",
+      map: (val: string) => {
+        if (!constants.acceptedMetadata.includes(val)) {
+          console.log(
+            "Metadata of type: " +
+              val +
+              " is not supported. Possible values are: classes/triggers/pages/components/aura/lwc"
+          );
+          process.exit();
+        } else {
+          return val;
+        }
+      }
+    }),
+    path: flags.string({
+      char: "p",
+      description: messages.getMessage("path"),
+      required: true
+    }),
+    sourceversion: flags.number({
+      char: "s",
+      description: messages.getMessage("sourceapi"),
+      min: 10,
+      max: 46
+    }),
+    targetversion: flags.number({
+      char: "t",
+      description: messages.getMessage("targetapi"),
+      min: 30,
+      max: 47
+    }),
+    fileprefix: flags.string({
+      char: "x",
+      description: messages.getMessage("prefix")
+    }),
+    dryrun: flags.boolean({
+      char: "d",
+      description: messages.getMessage("dryrun")
+    })
+  };
+
+  public async run(): Promise<void> {
+    this.ux.styledHeader(messages.getMessage("pluginTitle"));
+    this.ux.startSpinner("Running...");
+
+    // Generate the request object
+    let baseRequest: BaseRequest = {
       metadata: this.flags.metadata,
-      sourceversion: this.flags.sourceversion,
-      targetversion: this.flags.targetversion,
-      fileprefix: this.flags.fileprefix,
+      sourceversion: this.flags.sourceversion
+        ? this.flags.sourceversion
+        : constants.DEFAULT_SOURCE_API_VERSION,
+      targetversion: this.flags.targetversion
+        ? this.flags.targetversion
+        : constants.DEFAULT_TARGET_API_VERSION,
+      fileprefix: this.flags.fileprefix ? this.flags.fileprefix : "",
       path: this.flags.path,
-      dryrun: false
-		};
+      dryrun: this.flags.dryrun ? this.flags.dryrun : false
+    };
 
-    let service : ModifyMetaXMLFile = new ModifyMetaXMLFile();
-    service.applyProcess(baseRequest);
-
-    // Return an object to be displayed with --json
-    return {};
+    let service: ModifyMetaXMLFile = new ModifyMetaXMLFile();
+    service.applyProcess(baseRequest).then(() => {
+      this.ux.stopSpinner("Done!");
+    });
   }
 }
--- src/services/modifymetaxmlfile.ts
+++ src/services/modifymetaxmlfile.ts
@@ -1,128 +1,69 @@
-import {BaseRequest} from "./interfaces"
+import { BaseRequest } from "./interfaces";
 const replace = require("replace-in-file");
+const pathModule = require("path");
 export class ModifyMetaXMLFile {
-    async applyProcess(argv: BaseRequest): Promise<any> {
-        let prefix = "";
-        let searchForAPILowerThan = 46;
-        let replaceWithAPI = 47;
-        let dryRun = false;
-        let selectedFiles = [];
-        let errors = [];
-        let src = "";
+  async applyProcess(request: BaseRequest): Promise<any> {
+    let {
+      metadata,
+      sourceversion,
+      targetversion,
+      fileprefix,
+      path,
+      dryrun
+    } = request;
 
-        /*if (argv.helpme != null) {
-        console.log("This command accepts the following parameters:");
-        console.log("MANDATORY:");
-        console.log("--src path to your src folder followed by /");
-        console.log(
-            "--metadata classes/triggers/pages/components/aura/lwc. pick what you would like"
-        );
-        console.log("OPTIONAL:");
-        console.log(
-            "--fileprefix BP -> only files starting with BP* will be picked up. by default will take all files"
-        );
-        console.log(
-            "--apithreshold 35 -> only api version lower than 35 will be changed. default is 46"
-        );
-        console.log(
-            "--newapi 47 -> all files will be changed to api version 47. default is 47"
-        );
-        console.log("--dryrun true -> will do test-only. default is false");
-        return;
-        }*/
+    let fileName = `${fileprefix}*-meta.xml`;
+    let selectedFiles = [];
 
-        // validate and process the args
-        if (argv.fileprefix != null) {
-            prefix = argv.fileprefix;
-        }
-
-        if (argv.path == null) {
-            errors.push(
-                "--path has to be specified and it has to point to your src folder, followed by /"
-            );
-        } else {
-        src = argv.path;
-        }
-
-        if (argv.metadata == null) {
-            errors.push(
-                "--metadata has to be specified. possible values: classes/triggers/pages/components/aura/lwc"
-            );
-        } else {
-        let metadataList = argv.metadata.split("/");
-        for (let i = 0; i < metadataList.length; i++) {
-            if (metadataList[i] == "apex") {
-            selectedFiles.push(`${src}\\classes\\${prefix}*-meta.xml`);
-            }
-            if (metadataList[i] == "triggers") {
-            selectedFiles.push(`${src}\\triggers\\${prefix}*-meta.xml`);
-            }
-            if (metadataList[i] == "pages") {
-            selectedFiles.push(`${src}\\pages\\${prefix}*-meta.xml`);
-            }
-            if (metadataList[i] == "components") {
-            selectedFiles.push(`${src}\\components\\${prefix}*-meta.xml`);
-            }
-            if (metadataList[i] == "aura") {
-            selectedFiles.push(`${src}\\aura\\**\\${prefix}*-meta.xml`);
-            }
-            if (metadataList[i] == "lwc") {
-            selectedFiles.push(`${src}\\lwc\\**\\${prefix}*-meta.xml`);
-            }
-        }
-        }
-
-        if (argv.sourceversion != null) {
-        if (argv.sourceversion < 10 || argv.sourceversion > 46) {
-            errors.push("--apithreshold can be set between 10 and 46");
-        } else {
-            searchForAPILowerThan = argv.sourceversion;
-        }
-        }
-
-        if (argv.targetversion != null) {
-        if (argv.targetversion < 30 || argv.targetversion > 47) {
-            errors.push("--newapi can be set between 30 and 47");
-        } else {
-            replaceWithAPI = argv.targetversion;
-        }
-        }
-
-        if (errors.length != 0) {
-        errors.push("--helpme to see a list of possible arguments");
-        return console.log(errors);
-        }
-
-        if (argv.dryrun) {
-        dryRun = true;
-        }
-
-        // Set the Regex to match the max requested API version
-        let firstDigit = searchForAPILowerThan.toString()[0];
-        let secondDigit = searchForAPILowerThan.toString()[1];
-        let regex = `<apiVersion>([0-${+firstDigit - 1}][0-9]|[0-${firstDigit}][0-${+secondDigit}]).0</apiVersion>`;
-
-        let options = {
-        files: selectedFiles,
-        from: new RegExp(regex, "g"),
-        to: `<apiVersion>${replaceWithAPI}.0</apiVersion>`,
-        dry: dryRun, //set this to false to actually perform the changes
-        countMatches: true
-        };
-
-        console.log(options);
-
-        try {
-            let numberOfFilesChanged = 0;
-            const results = replace.sync(options);
-            for (let i = 0; i < results.length; i++) {
-                if (results[i].hasChanged == true) {
-                    numberOfFilesChanged += 1;
-                }
-            }
-            console.log(numberOfFilesChanged + " files have been changed!");
-        } catch (error) {
-            console.error("Error occurred:", error);
-        }
+    for (let i = 0; i < metadata.length; i++) {
+      if (metadata[i] == "classes") {
+        selectedFiles.push(pathModule.join(path, "classes", fileName));
+      }
+      if (metadata[i] == "triggers") {
+        selectedFiles.push(pathModule.join(path, "triggers", fileName));
+      }
+      if (metadata[i] == "pages") {
+        selectedFiles.push(pathModule.join(path, "pages", fileName));
+      }
+      if (metadata[i] == "components") {
+        selectedFiles.push(pathModule.join(path, "components", fileName));
+      }
+      if (metadata[i] == "aura") {
+        selectedFiles.push(pathModule.join(path, "aura", "**", fileName));
+      }
+      if (metadata[i] == "lwc") {
+        selectedFiles.push(pathModule.join(path, "lwc", "**", fileName));
+      }
     }
+
+    // Set the Regex to match the max requested API version
+    let firstDigit = sourceversion.toString()[0];
+    let secondDigit = sourceversion.toString()[1];
+    let regex = `<apiVersion>([0-${+firstDigit -
+      1}][0-9]|[${firstDigit}-${firstDigit}][0-${+secondDigit}]).0</apiVersion>`;
+
+    let options = {
+      files: selectedFiles,
+      from: new RegExp(regex, "g"),
+      to: `<apiVersion>${targetversion}.0</apiVersion>`,
+      dry: dryrun
+    };
+
+    console.log(options);
+
+    try {
+      let numberOfFilesChanged = 0;
+      const results = replace.sync(options);
+      for (let i = 0; i < results.length; i++) {
+        if (results[i].hasChanged == true) {
+          numberOfFilesChanged += 1;
+        }
+      }
+      console.log(numberOfFilesChanged + " files have been changed!");
+      return true;
+    } catch (error) {
+      console.error("Error occurred:", error);
+      return false;
+    }
+  }
 }
